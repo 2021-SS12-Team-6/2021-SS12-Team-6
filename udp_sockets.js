0a3d4f8e758ccb268a16e215c6135f7e70c6d473
Inviro <prelusiondilemma@gmail.com>
2021-01-30 17:54:17 -0800

    Removed modular code from the udp function

--- udp_sockets.js
+++ udp_sockets.js
@@ -1,27 +1,26 @@
-// Module used to handle sending UDP data using Javascript
-// export const name = 'udp_socks_js';
+async function sendRetrieveUDP(msg) {
+    const PORT_NUMBER = 200;
+    const HOST = 'localhost';
+    var udp = require('dgram');
+    var buffer = require('buffer'); // Used to read and write to buffers for UDP
+    var client = udp.createSocket('udp4'); // Creates UDP4 socket
 
-// export function 
+    var data = buffer.from(msg); // Converts msg into byte buffer
 
-var udp = require('dgram');
-var buffer = require('buffer'); // Used to read and write to buffers for UDP
-var client = udp.createSocket('udp4'); // Creates UDP socket
+    // When retrieving messages
+    client.on("message", (message, info) => {
+        console.log("Data recieved from server : " + message.toString());
+        console.log('Received %d bytes from %s:%d\n',message.length, info.address, info.port);
+        return message.toString();
+    });
 
-var DATA_TO_SEND = "Hello, World! Please change me";
-var data = Buffer.from(DATA_TO_SEND)
-
-// Getting message
-client.on("message", (message, info) => {
-    console.log("Data recieved from server : " + message.toString());
-    console.log('Received %d bytes from %s:%d\n',message.length, info.address, info.port);
-});
-
-// Sending message
-client.send(data, 200, 'localhost', (e) => {
-    if(e) {
-        console.log("Fatal error detected!");
-        client.close();
-    } else {
-        console.log("Message Sent!");
-    }
-})
+    // Sending message
+    client.send(data, PORT_NUMBER, HOST, (e) => {
+        if(e) {
+            console.log("Fatal error detected!");
+            client.close();
+        } else {
+            console.log("Message Sent!");
+        }
+    })
+}
