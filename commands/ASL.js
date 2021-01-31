9b53e143f77a7b26ecd2e48c54fe092981d63d43
Tomo Buchberg <tomobuchberg@gmail.com>
2021-01-30 17:51:15 -0800

    added command handler

---
+++ commands/ASL.js
@@ -0,0 +1,14 @@
+// 
+module.exports = function (message) {
+    // open UDP channel
+
+    // listen in on message channel
+
+    // send each message to parser
+
+    // process incoming image lists
+
+    // display image list
+    let images = "ASL";
+    message.channel.send(images);
+};
