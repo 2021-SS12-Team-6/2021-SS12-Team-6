58e2cafdd5afd6b5ac091815b5567b5d322277be
Inviro <prelusiondilemma@gmail.com>
2021-01-31 09:13:09 -0800

    Updated test cases, and did a few minor adjustments

--- parser.py
+++ parser.py
@@ -1,10 +1,10 @@
-import nltk                                 # Importing python natural language toolkit
-from nltk.tokenize import RegexpTokenizer   # Regular Expression Tokenizer
-from stopwords import get_stopwords
+import nltk                                         # Importing python natural language toolkit
+from nltk.tokenize import RegexpTokenizer           # Regular Expression Tokenizer
+from nltk.stem.wordnet import WordNetLemmatizer
+from spacy import *
 
 # Used for UDP transmission / receiving
 import socket
-# from threading import Thread
 
 
 class Parser:
@@ -12,8 +12,7 @@ class Parser:
         self.is_file = is_file
         self.path = path
         self.tokenizer = RegexpTokenizer(r'\w+')
-        self.stopwords = get_stopwords('english')
-        self.filtered_pos = ['CC', 'DT', 'VBP', 'VBZ']
+        self.filtered_pos = ['CC', 'DT', 'VBZ', 'TO']
         # UDP Socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Creating a TCP / IP socket
         self.server_address = ('localhost', 200)  # Tuple of address data
@@ -21,24 +20,88 @@ class Parser:
     def open_file(self):
         with open(self.path) as f:
             for line in f:  # For each line
-                print(self.parse_text(line))
+                temp = self.parse_text(line)
+                if temp:
+                    print(temp)
         f.close()
+
+    # def sort_text(self, input_arr):
+    #     result = [10] * len(input_arr)
+    #     last_was_in = False
+    #     ranking = {"PRP": 1, "PRP$": 1, "NN": 2, "VB": 3, "WP": 4}
+    #     print(list(map(lambda x: x[0], input_arr)))
+    #     print(list(map(lambda x: x[1], input_arr)))
+    #     for idx, word in enumerate(map(lambda x: x[1], input_arr)):
+    #         if word == "IN":
+    #             result[idx] = (0, input_arr[idx])
+    #             last_was_in = True
+    #             # print(result)
+    #             # print("in")
+    #         elif last_was_in:
+    #             result[idx] = (0, input_arr[idx])
+    #             last_was_in = False
+    #         else:
+    #             result[idx] = ranking.get(word, 10)
+    #         print(result)
+    #         # return sorted(result, key=lambda x: x[0])
+    #     # return sorted(input_arr)
+    #     # return result
+
+    # def sort_text(self, input_arr):
+    #     # (Time) Subject Verb Object
+    #     print(input_arr)
+    #     ranking = {
+    #         "IN": 0,
+    #         "PRP": 1,
+    #         "PRP$": 1,
+    #         "NN": 2,
+    #         "NNS": 2,
+    #         "NNP": 2,
+    #         "NNPS": 2,
+    #         "VB": 3,
+    #         "VBD": 3,
+    #         "VBG": 3,
+    #         "VBN": 3,
+    #         "VBP": 3,
+    #         "VBZ": 3,
+    #         "WDT": 4,
+    #         "WP": 4,
+    #         "WP$": 4,
+    #         "WRP": 4,
+    #     }
+    #     result = [[]] * 6
+    #     last_in = False
+    #     for word, pos in input_arr:
+    #         if pos != "IN":
+    #             if last_in:
+    #                 result[0].append(word)
+    #                 last_in = False
+    #             else:
+    #                 result[ranking.get(pos, 5)].append(word)
+    #         else:
+    #             result[0].append(word)
+    #             last_in = True
+    #     temp = []
+    #     # for arr in result:
+    #     #     temp.append(arr)
+    #     print(result)
 
     def parse_text(self, input_str: str):
         if input_str.strip():  # Line is not only whitespace
             # Tokenizes lines while removing punctuation and standardizing case
             tokens = self.tokenizer.tokenize(input_str)
             tokens = list(map(lambda x: x.lower(), tokens))
-            # tokens = list(filter(lambda x: x not in self.stopwords, tokens))
             tokens_pos = nltk.pos_tag(tokens)  # Checks parts of speech of each token
             # Filters unwanted parts of speech as well as converts back into a list of words
             filtered_tokens = filter(lambda x: x[1] not in self.filtered_pos, tokens_pos)
+            print(self.sort_text(list(filtered_tokens)))
+
             # -------Testing code-----------
-            # temp = list(filtered_tokens)
-            # print(temp)
+            temp = list(filtered_tokens)
+            print(temp)
             # print(tokens_pos)
             # -------Testing code-----------
-            filtered_tokens = map(lambda x: x[0], filtered_tokens)
+            filtered_tokens = map(lambda x: WordNetLemmatizer.lemmatize(x[0], 'v'), filtered_tokens)
             # Prints filtered list
             temp = list(filtered_tokens)
             if temp:
@@ -74,8 +137,8 @@ def main():
     p = Parser(is_file=True, path="testinput.txt")
     # On the first run of the program, call setup and download all-corpus
     # p.setup()
-    p.start_server()
-    # p.open_file()
+    # p.start_server()
+    p.open_file()
 
 
 main()
